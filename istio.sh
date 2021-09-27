fe382b1da68bc0609d14ae3ab4b6ecc247870ada
Abhishek Kumar <abhibvp003@gmail.com>
2021-09-27 10:01:46 +0530

    Update istio.sh

--- istio.sh
+++ istio.sh
@@ -2,23 +2,21 @@
 
 echo updating ubuntu
 sudo apt-get update
-
 echo upgrading ubuntu
 sudo apt-get upgrade -y
-
 echo installing dependencies for docker
 sudo apt-get -y install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg-agent \
-    software-properties-common
+		apt-transport-https \
+		ca-certificates \
+		curl \
+		gnupg-agent \
+		software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
- 
+	   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+	   $(lsb_release -cs) \
+	   stable"
+	 
 sudo apt-get update
 
 echo Installing Docker
@@ -28,40 +26,41 @@ sudo groupadd docker
 sudo usermod -aG docker $USER
 sudo chmod 666 /var/run/docker.sock
 
-curl -L https://istio.io/downloadIstio | sh -
-cd istio-1.11.1
+curl -L https://istio.io/downloadIstio | ISTIO_VERSION=1.11.2 TARGET_ARCH=x86_64 sh -
+cd istio-1.11.2
 sudo mv bin/istioctl /usr/local/bin/
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube
 minikube start
 
 istioctl install --set profile=demo -y
-
 cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
-br_netfilter
+	br_netfilter
 EOF
 
 cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
-net.bridge.bridge-nf-call-ip6tables = 1
-net.bridge.bridge-nf-call-iptables = 1
+	net.bridge.bridge-nf-call-ip6tables = 1
+	net.bridge.bridge-nf-call-iptables = 1
 EOF
 sudo sysctl --system
 
 sudo apt-get update && sudo apt-get install -y apt-transport-https curl
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
-deb https://apt.kubernetes.io/ kubernetes-xenial main
+	deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
 
-#sudo systemctl daemon-reload
-#sudo systemctl enable kubelet
-#sudo systemctl restart kubelet
-#kubeadm init
-#mkdir -p $HOME/.kube
-#cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-#chown $(id -u):$(id -g) $HOME/.kube/config
-#kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
-#kubeadm token create --print-join-command 
+	#sudo systemctl daemon-reload
+	#sudo systemctl enable kubelet
+	#sudo systemctl restart kubelet
+	#kubeadm init
+	#mkdir -p $HOME/.kube
+	#cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+	#chown $(id -u):$(id -g) $HOME/.kube/config
+	#kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
+	#kubeadm token create --print-join-command 
+	# sed -i 's/\r//' istio.sh
+
